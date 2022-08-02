cd479115e37b1d7cde309f29fe72f9e2ea86b55c
clqsrc <clq_work@163.com>
2022-08-02 16:07:09 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 // Import stylesheets
 import './style.css';
 
+//这个 index.ts 显然是默认加载的，不过 sz 里怎样手工生成 js 呢？
+
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+//appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-function f1(){
-  console.log("aaa")
-}//
-
-
+function f1() {
+  console.log('aaa');
+} //
