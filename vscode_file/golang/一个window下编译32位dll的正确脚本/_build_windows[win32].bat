4f01df6e32d9d5847299465050613e758232fefe
clqsrc <clq_work@163.com>
2022-10-30 13:35:15 +0800

    go

--- vscode_file/golang/一个window下编译32位dll的正确脚本/_build_windows[win32].bat
+++ vscode_file/golang/一个window下编译32位dll的正确脚本/_build_windows[win32].bat
@@ -12,6 +12,7 @@ PATH=D:\go1.10.8.windows-386\go\bin;D:\new\Qt\Qt5.12.12\Tools\mingw730_32\bin
 set GOARCH=386
 rem set GOARCH=amd64
 set GOOS=windows
+# CGO_ENABLED 目前也一定要有
 set CGO_ENABLED=1
 
 rem go build -buildmode=c-shared -o libgolang.dll http_dll.go
