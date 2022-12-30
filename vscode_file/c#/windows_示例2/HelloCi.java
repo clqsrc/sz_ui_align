05b7ca45ef6cf5e3a31d34f0589dc5a0ce7dcbd6
clqsrc <clq_work@163.com>
2022-12-30 13:55:14 +0800

    t

---
+++ vscode_file/c#/windows_示例2/HelloCi.java
@@ -0,0 +1,13 @@
+
+
+public static class HelloCi
+{
+    /// Returns a greeting message.
+    public static string GetMessage()
+    {
+        return "Hello, world!";
+    }
+
+    //public void
+
+}//
---
+++ vscode_file/c#/windows_示例2/main_t1.cs
@@ -0,0 +1,18 @@
+
+//本质上目前是 c# 语法
+//https://github.com/pfusik/cito
+//https://github.com/pfusik/cito/blob/master/doc/getting-started.md
+
+// C:\Users\ccc>where.exe dotnet
+// C:\Program Files\dotnet\dotnet.exe
+//https://github.com/OmniSharp/omnisharp-vscode/wiki/Troubleshooting:-'The-.NET-Core-SDK-cannot-be-located.'-errors
+
+public static class HelloCi
+{
+    /// Returns a greeting message.
+    public static string GetMessage()
+    {
+        return "Hello, world!";
+    }
+
+}//
