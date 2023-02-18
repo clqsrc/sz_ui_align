7fb03ed7aaaf0e3cc6ca776266de565bf843d977
clqsrc <clq_work@163.com>
2023-02-18 14:09:16 +0800

    t

--- 常用数据结构在多种语言中的表达/golang.go
+++ 常用数据结构在多种语言中的表达/golang.go
@@ -5,3 +5,15 @@
 
 //json
 
+
+//函数作为函数的参数
+
+//可以看到，直接写成函数的声明样式就可以了，只是不要原函数中的参数名
+
+func listenSSL(port int, f func(net.Conn)) {
+
+}
+
+func File_FindFiles_onfunc(path string, out_list * []map[string]string, find_sub bool, file_type string, on_func func(string)bool)  {
+}
+
